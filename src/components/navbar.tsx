a170626aae3263e7f6ee00b5054a7a8709f3c277
ashkar2023 <muhammedashkar2023@gmail.com>
2026-01-02 21:55:20 +0530

    ignored tsc error

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 
+// @ts-ignore
 const searchIcon = <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
 
 export const Navbar = () => {
